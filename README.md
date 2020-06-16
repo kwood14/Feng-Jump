3a42493527c0bb854e3b7572918c6eed75e5ea89
Kai <kwood14@asu.edu>
2020-06-16 11:50:55 -0700

    Initial files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Feng Jump
+### Currently in progress 
+### Written by Kai Wood, June 2020
+
+# To run:
+### 1. Clone the repo
+### 2. Open git in the folder
+#### 3. gradle build
+#### 4. gradle run
+#### or
+#### 3. gradle fatJar
+#### 4. Then you can find the exectuable jar in the build/libs folder
---
+++ src/main/java/Start.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Created by Kai Wood
+ */
+public class Start {
+
+
+    public static void main(String[] args) {
+   	
+    }
+
+}
