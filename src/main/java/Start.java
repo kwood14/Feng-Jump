36ba1f8da5ee5d131479153174b8bbc0cbf791c4
Kai <kwood14@asu.edu>
2020-06-16 14:17:17 -0700

    Added a menu

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -5,11 +5,14 @@ import java.awt.FlowLayout;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
@@ -27,6 +30,7 @@ public class Start {
         JFrame frame = new JFrame("Feng Jump Beta V1.0");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 420);
+        frame.setJMenuBar(menu());
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         JTextArea outputTextArea = new JTextArea(5,5);
@@ -46,9 +50,6 @@ public class Start {
 
         JPanel heroControlPanel = new JPanel(new BorderLayout());
         heroControlPanel.add(buttonPanel, BorderLayout.NORTH);
-        
-        //---------------------------------------------------
-
 
         JPanel mainPanel = new JPanel(new BorderLayout());
         mainPanel.add(heroControlPanel, BorderLayout.CENTER);
@@ -60,25 +61,9 @@ public class Start {
         frame.setLayout(new BorderLayout());
         frame.add(mainPanel, BorderLayout.CENTER);
         frame.setSize(new Dimension(800, 650));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setVisible(true);
-        
-        JMenuBar menubar = new JMenuBar();
-        JMenu home = new JMenu("Home");
-        JMenuItem choose = new JMenuItem("Pick a Character");
-        JMenu about = new JMenu("About");
-        JMenuItem credit = new JMenuItem("Author");
-        home.add(choose);
-        about.add(credit);
-        menubar.add(home);
-        menubar.add(about);
-        frame.setJMenuBar(menubar);
 
         //Button Listeners
         startButton.addActionListener((e) -> {
-            //String english = outputTextArea.getText().trim();
             outputTextArea.setText("Start Selected");
         });
 
@@ -91,6 +76,54 @@ public class Start {
         }
         
         frame.setVisible(true);
+    }
+    
+    public JMenuBar menu()
+    {
+        // Create a MenuBar
+        JMenuBar menu = new JMenuBar();
+        
+        // Create a Game menu
+        JMenu game = new JMenu("Game");
+
+        // Close out the screen
+        JMenuItem close = new JMenuItem("Close");
+        close.addActionListener((ActionEvent e) -> {
+            System.exit(0);
+        });
+        JMenuItem restart = new JMenuItem("Restart");
+        
+        game.add(restart);
+        game.add(close);
+        
+
+        // Create a Help menu
+        JMenu help = new JMenu("Help");
+
+        JMenuItem about = new JMenuItem("About");
+        about.addActionListener((e) -> {
+            JOptionPane.showMessageDialog(null, "<html><strong>Feng Jump Beta V1.0</strong><br><br> "
+                    + "Developed by Kai Wood<br><br>"
+                    + "<br></html>", "About", 1);
+        });
+        
+        JMenuItem htp = new JMenuItem("How to Play");
+        htp.addActionListener((e) -> {
+            JOptionPane.showMessageDialog(null, "<html>"
+                    + "Up - <strong>W</strong><br>"
+                    + "Down - <strong>S</strong><br>"
+                    + "Left - <strong>A</strong><br>"
+                    + "Right - <strong>D</strong><br><br>"
+                    + "</html>", "How to play", JOptionPane.QUESTION_MESSAGE);
+        });
+
+        help.add(htp);
+        help.add(about);
+
+        menu.add(game);
+        menu.add(help);
+
+        return menu;
     }
     public static void main(String[] args) {
         try {
