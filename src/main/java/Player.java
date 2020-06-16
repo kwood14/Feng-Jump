e3868e155dce80ba3b1d2d065e974bc8e4d9e07d
Kai <kwood14@asu.edu>
2020-06-16 15:56:36 -0700

    Added Player class

---
+++ src/main/java/Player.java
@@ -0,0 +1,50 @@
+import java.awt.Image;
+
+
+public class Player {
+    private int x, y;
+    private int dx, dy;
+    private boolean dead;
+    private Image playerIcon;
+
+    public Player() {
+    }
+    
+    public Image getPlayerIcon() {
+        return playerIcon;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setDx(int d) {
+        this.dx = d;
+    }
+
+    public void setDy(int d) {
+        this.dy = d;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int num) {
+
+        this.y = num;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
+
+}
